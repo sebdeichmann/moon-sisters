d918e6ffc4c4566c43ba13d1993a99141d119318
Jana Webdesign <sebastian.deichmann@proton.me>
2026-07-09 12:46:28 +0200

    feat: add Git-based CMS content setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { siteContent } from './content/siteContent.js';
+import siteContent from './content/siteContent.json';
 import './styles/global.css';
 import App from './App.jsx';
 
